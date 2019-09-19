86ad22b64ae0134c75cd5a4a9bcfaadb7efe81d9
Xander C <xander.canedo@gmail.com>
2019-09-18 23:26:13 -0600

    functioning game after talking with classmates
    git status

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,181 @@
-var questions = [
-    {
-        question: "In Super Mario Bros, what is Mario's brother's name?",
-        choices: ["Sparky", "Larry", "Indiana", "Luigi"],
-        answer: 3
-    },
-    {
-        question: "Who is Sonic The Hedgehog's sidekick?",
-        choices:["Tails","Smiles","Knuckles","Dr. Robutnik"],
-        answer: 0
-    }
-]
-
-//  Set our number counter to 10.
-var number = 10;
-
-//  Variable that will hold our interval ID when we execute
-//  the "run" function
-var intervalId;
-
-// when click on start, run start function
-$("#start").on("click", run);
-
-// the actual run function
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-}
-
-//  The decrement function.
-function decrement() {
-    //  Decrease number by one.
-    number--;
-    //  Show the number in the #show-number tag.
-    $("#timer").html("<h2>" + "Time remaining: " + number + "</h2>");
-    //  Once number hits zero...
-    if (number === 0) {
-        //  ...run the stop function.
-        stop();
-        //  Alert the user that time is up.
-        alert("Time Up!");
-    }
-}
-
-//  The stop function
-function stop() {
-    //  Clears our intervalId
-    //  We just pass the name of the interval
-    //  to the clearInterval function.
-    clearInterval(intervalId);
-}
-
-function displayQuestion() {
+$(document).ready(function(){
+  
+   
+    $("#timeLeft").hide();
+    $("#begin").on('click', trivia.beginGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
     
-    for (var i = 0; i < questions.length; i++) {
-        $("#questions").append("<div class='row'>" + "<div class='col-md-12'>"  +questions[i].question  + "</div>" + "</div>")
-        console.log(questions[i].question)
-    }
-}
+  })
+  
+  var trivia = {
+    
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : '',
+ 
+    questions: {
+      q1: 'What is the name of marios brother?',
+      q2: 'Off what country are the islands of Islay, Mull, and St. Kilda located?',
+      q3: 'Which U.S. president appears on the front of the $2 bill?',
+      q4: 'Which U.S. state has the longest coastline?',
+      q5: 'BB-8 is an astromech droid from what film?',
+      q6: 'In the movie "The Wizard of Oz", what did the Scarecrow want from the wizard?',
+      q7: 'What is the first element on the periodic table?'
+    },
+    options: {
+      q1: ['Atlanta', 'New Delhi', 'Mumbai', 'Singapore'],
+      q2: ['Scotland', 'Ireland', 'Iceland', 'France'],
+      q3: ['Thomas Jefferson', 'George Bush', 'Barack Obama', 'Donald Trump'],
+      q4: ['Alaska', 'California', 'New York', 'Texas'],
+      q5: ['When Harry Met Sally','Harry Potter & The Deathly Hallows','Star Wars','Terminator'],
+      q6: ['Brains','Books','Beer','Birds'],
+      q7: ['Oxygen', 'Hydrogen', 'Nitrogen','Carbon']
+    },
+    answers: {
+      q1: 'New Delhi',
+      q2: 'Scotland',
+      q3: 'Thomas Jefferson',
+      q4: 'Alaska',
+      q5: 'Star Wars',
+      q6: 'Brains',
+      q7: 'Hydrogen'
+    },
+    
+    beginGame: function(){
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      
+      $('#game').show();
+      
 
-displayQuestion()
+      $('#answers').html('');
+      
+      
+      $('#timer').text(trivia.timer);
+      
+      
+      $('#begin').hide();
+  
+      $('#timeLeft').show();
+      
+      
+      trivia.nextQuestion();
+      
+    },
+     
+    nextQuestion : function(){
+      
+      
+      trivia.timer = 10;
+       $('#timer').removeClass('last-seconds');
+      $('#timer').text(trivia.timer);
+      
+     
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $('#question').text(questionContent);
+      
+      
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+     
+      $.each(questionOptions, function(index, key){
+        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+      })
+      
+    },
+    
+    timerRunning : function(){
+      
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $('#timer').text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $('#timer').addClass('last-seconds');
+          }
+      }
+     
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#answers').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      }
+      
+      else if(trivia.currentSet === Object.keys(trivia.questions).length){
+        
+        
+        $('#answers')
+          .html('<h3>Thank you for playing!</h3>'+
+          '<p>Correct: '+ trivia.correct +'</p>'+
+          '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+          '<p>Please play again!</p>');
+        
+        
+        $('#game').hide();
+        
+       
+        $('#begin').show();
+      }
+      
+    },
+    
+    guessChecker : function() {
+      
+      
+      var resultId;
+      
+      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+      
+
+      if($(this).text() === currentAnswer){
+       
+        $(this).addClass('btn-success').removeClass('btn-info');
+        
+        trivia.correct++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#answers').html('<h3>Correct Answer!</h3>');
+      }
+      
+      else{
+       
+        $(this).addClass('btn-danger').removeClass('btn-info');
+        
+        trivia.incorrect++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#answers').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+      }
+      
+    },
+   
+    guessResult : function(){
+      
+   
+      trivia.currentSet++;
+      
+    
+      $('.option').remove();
+      $('#answers h3').remove();
+      
+     
+      trivia.nextQuestion();
+       
+    }
+  
+  }
