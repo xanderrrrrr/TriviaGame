96c0601eabdba0cd28dbb1fdbccbd2d88b192c13
Xander C <xander.canedo@gmail.com>
2019-09-18 23:50:30 -0600

    have working game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,181 +1,137 @@
-$(document).ready(function(){
-  
-   
-    $("#timeLeft").hide();
-    $("#begin").on('click', trivia.beginGame);
-    $(document).on('click' , '.option', trivia.guessChecker);
-    
-  })
-  
-  var trivia = {
-    
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-    timer: 20,
-    timerOn: false,
-    timerId : '',
- 
-    questions: {
-      q1: 'What is the name of marios brother?',
-      q2: 'Off what country are the islands of Islay, Mull, and St. Kilda located?',
-      q3: 'Which U.S. president appears on the front of the $2 bill?',
-      q4: 'Which U.S. state has the longest coastline?',
-      q5: 'BB-8 is an astromech droid from what film?',
-      q6: 'In the movie "The Wizard of Oz", what did the Scarecrow want from the wizard?',
-      q7: 'What is the first element on the periodic table?'
-    },
-    options: {
-      q1: ['Atlanta', 'New Delhi', 'Mumbai', 'Singapore'],
-      q2: ['Scotland', 'Ireland', 'Iceland', 'France'],
-      q3: ['Thomas Jefferson', 'George Bush', 'Barack Obama', 'Donald Trump'],
-      q4: ['Alaska', 'California', 'New York', 'Texas'],
-      q5: ['When Harry Met Sally','Harry Potter & The Deathly Hallows','Star Wars','Terminator'],
-      q6: ['Brains','Books','Beer','Birds'],
-      q7: ['Oxygen', 'Hydrogen', 'Nitrogen','Carbon']
-    },
-    answers: {
-      q1: 'New Delhi',
-      q2: 'Scotland',
-      q3: 'Thomas Jefferson',
-      q4: 'Alaska',
-      q5: 'Star Wars',
-      q6: 'Brains',
-      q7: 'Hydrogen'
-    },
-    
-    beginGame: function(){
-      trivia.currentSet = 0;
-      trivia.correct = 0;
-      trivia.incorrect = 0;
-      trivia.unanswered = 0;
-      clearInterval(trivia.timerId);
-      
-      
-      $('#game').show();
-      
+$(document).ready(function () {
 
-      $('#answers').html('');
-      
-      
-      $('#timer').text(trivia.timer);
-      
-      
-      $('#begin').hide();
-  
-      $('#timeLeft').show();
-      
-      
-      trivia.nextQuestion();
-      
-    },
-     
-    nextQuestion : function(){
-      
-      
-      trivia.timer = 10;
-       $('#timer').removeClass('last-seconds');
-      $('#timer').text(trivia.timer);
-      
-     
-      if(!trivia.timerOn){
-        trivia.timerId = setInterval(trivia.timerRunning, 1000);
-      }
-      
-      
-      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
-      $('#question').text(questionContent);
-      
-      
-      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
-      
-     
-      $.each(questionOptions, function(index, key){
-        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
-      })
-      
-    },
-    
-    timerRunning : function(){
-      
-      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
-        $('#timer').text(trivia.timer);
-        trivia.timer--;
-          if(trivia.timer === 4){
-            $('#timer').addClass('last-seconds');
+  //the game start when pressing start btn
+  $('.start-btn').click(function () {
+
+      //hide start button and clear the div
+      $('.start-btn').hide();
+      $('.quiz-section').empty();
+
+      // Generate quiz page with jquery
+      // quiz items
+      var quiz = {
+          set1: {
+              question: "What is the name of Marios brother?",
+              answer: ['Sparky', 'Larry', 'Luigi', 'Indiana', "ans1"],
+              correct: "Luigi"
+          },
+          set2: {
+              question: "What is Sonics sidekick name?",
+              answer: ['Smiles', 'Tails', 'Knuckles', 'Robutnik', "ans2"],
+              correct: "Tails"
+          },
+          set3: {
+              question: "What is the name of the main protaganist in the franchise Tomb Raider?",
+              answer: ['Tara', 'Laura', 'Christie', 'Lara', "ans3"],
+              correct: "Lara"
+          },
+          set4: {
+              question: "What was the first expansion for World of Warcraft?",
+              answer: ['Wrath of the Lich King','The Burning Crusade','Pandaland','Legion', "ans4"],
+              correct: "The Burning Crusade"
+          },
+          set5: {
+              question: "What is the name of the evil corporation in Resident Evil?",
+              answer: ['Labcorp','Aperture Labs','Shinra Electric Power Co.','Umbrella Corp', "ans5"],
+              correct: "Umbrella Corp"
+          },
+          set6: {
+              question: "What is the best Halo game?",
+              answer: ['Halo: Combat Evolved', 'Halo 2', 'Halo 4', 'Halo 5', "ans6"],
+              correct: "Halo 2"
+          },
+          set7: {
+              question: "Which seventh generation handheld gaming console was released in North America on March 24, 2005?",
+              answer: ['PSP', 'Switch', 'DSi','Ngage', "ans7"],
+              correct: "PSP"
           }
-      }
-     
-      else if(trivia.timer === -1){
-        trivia.unanswered++;
-        trivia.result = false;
-        clearInterval(trivia.timerId);
-        resultId = setTimeout(trivia.guessResult, 1000);
-        $('#answers').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
-      }
-      
-      else if(trivia.currentSet === Object.keys(trivia.questions).length){
-        
-        
-        $('#answers')
-          .html('<h3>Thank you for playing!</h3>'+
-          '<p>Correct: '+ trivia.correct +'</p>'+
-          '<p>Incorrect: '+ trivia.incorrect +'</p>'+
-          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
-          '<p>Please play again!</p>');
-        
-        
-        $('#game').hide();
-        
-       
-        $('#begin').show();
-      }
-      
-    },
-    
-    guessChecker : function() {
-      
-      
-      var resultId;
-      
-      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
-      
 
-      if($(this).text() === currentAnswer){
-       
-        $(this).addClass('btn-success').removeClass('btn-info');
-        
-        trivia.correct++;
-        clearInterval(trivia.timerId);
-        resultId = setTimeout(trivia.guessResult, 1000);
-        $('#answers').html('<h3>Correct Answer!</h3>');
+      };
+
+      //timer text
+      $('.quiz-section').append('<div class="timer"><h2>You have <span class="time-Text">60</span> second to answer!</h2></div>');
+
+      
+      //Timer
+      var isTimerOn = false;
+      var timeCountDown = 60; // 1mins
+      if (isTimerOn == false) {
+          var timer = setInterval(function () {
+              timeCountDown--;
+              $('.time-Text').text(timeCountDown);
+              if (timeCountDown <= 0) {
+                  getResult(); //time out, get result
+              }
+          }, 1000);
+          isTimerOn = true;
       }
-      
-      else{
-       
-        $(this).addClass('btn-danger').removeClass('btn-info');
-        
-        trivia.incorrect++;
-        clearInterval(trivia.timerId);
-        resultId = setTimeout(trivia.guessResult, 1000);
-        $('#answers').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+
+
+      //quiz layout
+      // ansArray to take argument from sub-sets            
+      var ansArray = [];
+      for (var set in quiz) {
+          //Correct Ans array for comparison
+          ansArray.push(quiz[set].correct);
+          //Display question
+          $(".quiz-section").append('<br><p>' + quiz[set].question + '</p><br>');
+          for (var i = 0; i < 4; i++) { // first 4 (0,1,2,3) arguments are answers
+              var addInput = $("<input>");
+              addInput.addClass("radioChoice");
+              addInput.attr("type", 'radio');
+              addInput.attr("name", quiz[set].answer[4]);
+              addInput.attr("value", quiz[set].answer[i]);
+              addInput.text(quiz[set].answer[i]);
+              //Display choices
+              $(".quiz-section").append(addInput);
+              $(".quiz-section").append('<p style="display:inline">' + quiz[set].answer[i] + '</p><br>');
+          }
+      };
+
+      //generate submit button
+      var addSubmitBtn = $("<button>");
+      addSubmitBtn.addClass("button btn-success btn-lg submit-btn");
+      addSubmitBtn.text('Submit');
+      $(".quiz-section").append('<br>');
+      $(".quiz-section").append(addSubmitBtn);
+
+      //generate an array to store user's choice
+      var userArray = [];
+
+      //submit button
+      $('.submit-btn').click(function () {
+          getResult();
+      });
+
+      //score calculation
+      var correctCount;
+      var skippedCount;
+      var wrongCount;
+      //time up and submit will trigger this function
+      function getResult() {
+          correctCount = 0;
+          var getCheckedInput = $('Input:checked');
+          $('Input:checked').each(function () {
+              userArray.push($(this).val());
+          });
+          // Compare userArray and ansArray using indexOf
+          for (var i = 0; i < userArray.length; i++) {
+              if (ansArray.indexOf(userArray[i]) > -1) {
+                  correctCount++;
+              }
+          }
+          skippedCount = ansArray.length - userArray.length;
+          wrongCount = ansArray.length - correctCount - skippedCount;
+
+          // result page looks like
+          $('.quiz-section').empty();
+          $('.quiz-section').append("<p> Correct:" + correctCount + "</p>");
+          $('.quiz-section').append("<p> Wrong:" + wrongCount + "</p>");
+          $('.quiz-section').append("<p> Skipped:" + skippedCount + "</p>");
+          $('.quiz-section').append("<p> Try Again!</p>");
+          $('.start-btn').show();
+          isTimerOn == false;
+          clearInterval(timer);
       }
-      
-    },
-   
-    guessResult : function(){
-      
-   
-      trivia.currentSet++;
-      
-    
-      $('.option').remove();
-      $('#answers h3').remove();
-      
-     
-      trivia.nextQuestion();
-       
-    }
-  
-  }
+  });
+})
